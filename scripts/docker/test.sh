ddd490f0bcad1eb9a9f72c759c4437ee23348249
ari <ari@mascii.org>
2020-03-04 00:02:51 -0500

    test docker mscore

--- scripts/docker/test.sh
+++ scripts/docker/test.sh
@@ -1,2 +1,4 @@
-docker run --rm -it -v $PWD/testio:/testio mscore-3 -o /testio/test.xml /testio/test.mid
+#docker run --rm -it -v $PWD/testio:/testio mscore-3 -o /testio/test.xml /testio/test.mid
+#docker run --rm -it -v $PWD/testio:/testio mscore-3 -o /testio/test.mp3 /testio/test.mid
+docker run --rm -it -v $PWD/testio:/testio mscore-3 -o /testio/test.png /testio/test.mid
 #docker run --rm -it --entrypoint /bin/bash mscore-3 

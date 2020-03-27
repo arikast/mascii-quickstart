4bb8975f82a19bc7f6db8e6b8a64e95f69fbe9d0
ari <ari@mascii.org>
2020-03-27 19:27:02 -0400

    better soundfonts

--- docker/timidity/test.sh
+++ docker/timidity/test.sh
@@ -1,3 +1,3 @@
-docker run --rm -it -v $PWD/testio:/wrk timidity /wrk/bach-air-on-g-string.mid /wrk/bach-air-on-g-string.mp3 
+docker run --rm -it -v $PWD/conf/timidity.cfg:/etc/timidity/timidity.cfg -v $PWD/testio:/wrk timidity /wrk/bach-air-on-g-string.mid /wrk/bach-air-on-g-string.mp3 
 #docker run --rm -it --entrypoint /bin/bash timidity
 
